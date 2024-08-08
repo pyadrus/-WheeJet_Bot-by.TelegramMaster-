6dd3027d4f52c4a82fda7442e6ce1e2b915a756c
@PyAdminRU <zh.vitaliy92@gmail.com>
2024-08-08 23:02:13 +0300

    Добавлена ссылка на канал

--- handlers/user/user_handlers.py
+++ handlers/user/user_handlers.py
@@ -30,7 +30,6 @@ async def instructions_handlers(callback_query: types.CallbackQuery) -> None:
     user_name = callback_query.from_user.username
     user_first_name = callback_query.from_user.first_name
     user_last_name = callback_query.from_user.last_name
-    # user_date = callback_query.date.strftime("%Y-%m-%d %H:%M:%S")
     logger.info(f"{user_id} {user_name} {user_first_name} {user_last_name}")
     sign_up_text = "Добро пожаловать user"
     greeting_keyboards = greeting_keyboard()
--- keyboards/keyboards.py
+++ keyboards/keyboards.py
@@ -6,7 +6,7 @@ def greeting_keyboard():
         [InlineKeyboardButton(text='Связь с поддержкой', callback_data='contact_support'),
          InlineKeyboardButton(text='Инструкция', callback_data='instructions')],
         [InlineKeyboardButton(text='Гарантия', callback_data='check_out_the_warranty'),
-         InlineKeyboardButton(text='Перейти в канал', callback_data='go_to_channel')],
+         InlineKeyboardButton(text='Перейти в канал', url='https://t.me/wheejet_ru')],
     ]
     greeting_keyboards = InlineKeyboardMarkup(inline_keyboard=rows)
     return greeting_keyboards
