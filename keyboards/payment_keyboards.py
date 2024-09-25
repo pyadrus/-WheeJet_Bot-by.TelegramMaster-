299dbb1f4204d5f35e3e93eda9d864d59e255268
@PyAdminRU <zh.vitaliy92@gmail.com>
2024-09-26 00:01:27 +0300

    Добавлена оплата гарантии на 2 года.

--- keyboards/payment_keyboards.py
+++ keyboards/payment_keyboards.py
@@ -12,13 +12,23 @@ def back_to_main_menu_keyboard_garan():
 
 
 
-def extended_warranty_2_years_continue_keyboard():
+def extended_warranty_2_years_continue_keyboard(url, id_pay):
+    """
+    Клавиатура для продолжения оплаты
+    :param url: url для перехода на страницу оплаты
+    :param id_pay: id оплаты (проверка оплаты)
+    """
+
     rows = [
-        [InlineKeyboardButton(text='Продолжить', callback_data='extended_warranty_2_years_continue')],
+        [
+            InlineKeyboardButton(text='Продолжить', url=url),
+            InlineKeyboardButton(text='Проверить оплату', callback_data=f"check_service_{id_pay}"),
+        ],
         [InlineKeyboardButton(text='🔙 Назад в меню', callback_data='back_to_menu')]
     ]
     check_the_warranty_card_keyboards = InlineKeyboardMarkup(inline_keyboard=rows)
     return check_the_warranty_card_keyboards
+
 
 def extended_warranty_3_years_continue_keyboard():
     rows = [
